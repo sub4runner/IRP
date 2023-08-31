4ba2f15edb224fadf8e0b15fd4a599f24878a1d3
gu <dkrehddl2001@gmail.com>
2023-08-31 10:13:35 +0900

    2nd commit

--- src/com/test/User.java
+++ src/com/test/User.java
@@ -3,7 +3,8 @@ package com.test;
 public class User {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		long num = 100_0000_0000L;
+		System.out.println(num);
 
 	}
 
